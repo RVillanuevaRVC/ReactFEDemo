5fc41899d75a6c9b41bf7d4f84529c7ef6753342
RVillanuevaRVC <75404849+RVillanuevaRVC@users.noreply.github.com>
2020-12-19 08:04:26 +0700

    Update tReactMathjax.js

--- tReactMathjax.js
+++ tReactMathjax.js
@@ -1,3 +1,4 @@
 //react-mathjax
 //https://github.com/wko27/react-mathjax/
 //blog: https://medium.com/@sjorsvanheuveln/displaying-math-in-reactjs-using-es6-5b0739e8447e
+//https://stackoverflow.com/questions/45106453/rendering-math-in-react-component
